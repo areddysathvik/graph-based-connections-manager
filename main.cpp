e7107b21cd7609355f30e18d84b5d7357a102795
areddysathvik <areddisathvik@gmail.com>
2025-04-12 12:19:01 +0530

    ADD Some Fn
    ADD Some Fn 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+void do_something(){
+    cout << "Doing Something";
+}
+
+void do_something_2(){
+    cout << "Doing Something";
+}
+
 int main(){
+    do_something();
     return 0;
 }
